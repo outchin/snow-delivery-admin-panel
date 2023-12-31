255943566c1efc9c375116d6754074c07eb99596
zawwaisoe-lastoenjoy <zawwaisoe.lastoenjoy@gmail.com>
2023-12-31 12:03:25 +0630

    add meta

--- resources/views/layouts/landing/app.blade.php
+++ resources/views/layouts/landing/app.blade.php
@@ -14,21 +14,22 @@
 <html dir="{{ $landing_site_direction }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('public/assets/landing/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('public/assets/landing/css/customize-animate.css') }}" />
-    <link rel="stylesheet" href="{{ asset('public/assets/landing/css/odometer.css') }}" />
-    <link rel="stylesheet" href="{{ asset('public/assets/landing/css/owl.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('public/assets/admin/css/toastr.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/assets/landing/css/main.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/assets/landing/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/landing/css/customize-animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/landing/css/odometer.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/landing/css/owl.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/admin/css/toastr.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/landing/css/main.css') }}"/>
 
     @php($icon = \App\Models\BusinessSetting::where(['key' => 'icon'])->first()->value ?? '')
-    <link rel="icon" type="image/x-icon" href="{{ asset('storage/app/public/business/' . $icon ?? asset('public/assets/landing/img/favicon.svg')) }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/app/business/' . $icon ?? asset('/assets/landing/img/favicon.svg')) }}">
     @stack('css_or_js')
     @php($backgroundChange = \App\Models\BusinessSetting::where(['key' => 'backgroundChange'])->first())
     @php($backgroundChange = isset($backgroundChange) && $backgroundChange->value ? json_decode($backgroundChange->value,true):'')
@@ -61,8 +62,8 @@
                     @php($logo = \App\Models\BusinessSetting::where(['key' => 'logo'])->first()->value ?? '')
                     <a href="{{route('home')}}" class="logo">
                         <img
-                        onerror="this.src='{{ asset('public/assets/admin/img/160x160/img2.jpg') }}'"
-                    src="{{ asset('storage/app/public/business/' . $fav) }}" alt="">
+                        onerror="this.src='{{ asset('/assets/admin/img/160x160/img2.jpg') }}'"
+                    src="{{ asset('storage/app//business/' . $fav) }}" alt="">
                     </a>
                     <ul class="menu">
                         <li>
@@ -209,8 +210,8 @@
                     <div class="footer-widget">
                         <div class="footer-logo">
                             <a class="logo">
-                                <img onerror="this.src='{{ asset('public/assets/admin/img/160x160/img2.jpg') }}'"
-                            src="{{ asset('storage/app/public/business/' . $logo) }}" alt="">
+                                <img onerror="this.src='{{ asset('/assets/admin/img/160x160/img2.jpg') }}'"
+                            src="{{ asset('storage/app//business/' . $logo) }}" alt="">
                             </a>
                         </div>
                         <div class="txt">
@@ -222,7 +223,7 @@
                                 @foreach ($social_media as $social)
                                 <li>
                                     <a href="{{ $social->link }}" target="_blank">
-                                        <img src="{{ asset('public/assets/landing/img/footer/'. $social->name.'.svg') }}" alt="">
+                                        <img src="{{ asset('/assets/landing/img/footer/'. $social->name.'.svg') }}" alt="">
                                     </a>
                                 </li>
                                 @endforeach
@@ -234,12 +235,12 @@
                         <div class="app-btn-grp">
                             @if (isset($landing_page_links['playstore_url_status']))
                             <a href="{{ isset($landing_page_links['playstore_url']) ? $landing_page_links['playstore_url'] : '' }}">
-                                <img src="{{ asset('public/assets/landing/img/google.svg') }}" alt="">
+                                <img src="{{ asset('/assets/landing/img/google.svg') }}" alt="">
                             </a>
                             @endif
                             @if (isset($landing_page_links['apple_store_url_status']))
                             <a href="{{ isset($landing_page_links['apple_store_url']) ? $landing_page_links['apple_store_url'] : '' }}">
-                                <img src="{{ asset('public/assets/landing/img/apple.svg') }}" alt="">
+                                <img src="{{ asset('/assets/landing/img/apple.svg') }}" alt="">
                             </a>
                             @endif
                         </div>
@@ -324,14 +325,14 @@
         </div>
     </footer>
     <!-- ======= Footer Section ======= -->
-    <script src="{{ asset('public/assets/landing/js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/viewport.jquery.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/wow.min.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/odometer.min.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/owl.min.js') }}"></script>
-    <script src="{{ asset('public/assets/landing/js/main.js') }}"></script>
-    <script src="{{ asset('public/assets/admin') }}/js/toastr.js"></script>
+    <script src="{{ asset('/assets/landing/js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/viewport.jquery.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/wow.min.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/odometer.min.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/owl.min.js') }}"></script>
+    <script src="{{ asset('/assets/landing/js/main.js') }}"></script>
+    <script src="{{ asset('/assets/admin') }}/js/toastr.js"></script>
     {!! Toastr::message() !!}
     @if ($errors->any())
         <script>

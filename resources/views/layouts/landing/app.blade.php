4ce1ab4783f2945e46f7c818fd2e05a061821125
zawwaisoe-lastoenjoy <zawwaisoe.lastoenjoy@gmail.com>
2023-12-31 13:42:39 +0630

    add meta 2

--- resources/views/layouts/landing/app.blade.php
+++ resources/views/layouts/landing/app.blade.php
@@ -15,10 +15,14 @@
 <html dir="{{ $landing_site_direction }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <meta charset="UTF-8" />
+{{--    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">--}}
+{{--    <meta charset="UTF-8" />--}}
 {{--    <meta http-equiv="X-UA-Compatible" content="IE=edge" />--}}
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1.0" />--}}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>@yield('title')</title>
 

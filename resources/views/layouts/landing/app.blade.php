958572b2fecaaa0009164b578a3b571e13dc445c
zawwaisoe-lastoenjoy <zawwaisoe.lastoenjoy@gmail.com>
2024-01-02 13:36:01 +0630

    fix with cdn

--- resources/views/layouts/landing/app.blade.php
+++ resources/views/layouts/landing/app.blade.php
@@ -19,19 +19,27 @@
 {{--    <meta charset="UTF-8" />--}}
 {{--    <meta http-equiv="X-UA-Compatible" content="IE=edge" />--}}
 {{--    <meta name="viewport" content="width=device-width, initial-scale=1.0" />--}}
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
     <!-- Required Meta Tags Always Come First -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('/assets/landing/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/landing/css/customize-animate.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/landing/css/odometer.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/landing/css/owl.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/admin/css/toastr.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/landing/css/main.css') }}"/>
+{{--    <link rel="stylesheet" href="{{ asset('/assets/landing/css/bootstrap.min.css') }}" />--}}
+{{--    <link rel="stylesheet" href="{{ asset('/assets/landing/css/customize-animate.css') }}" />--}}
+{{--    <link rel="stylesheet" href="{{ asset('/assets/landing/css/odometer.css') }}" />--}}
+{{--    <link rel="stylesheet" href="{{ asset('/assets/landing/css/owl.min.css') }}" />--}}
+{{--    <link rel="stylesheet" href="{{ asset('/assets/admin/css/toastr.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('/assets/landing/css/main.css') }}"/>--}}
+
+
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/landing/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/landing/css/customize-animate.css" />
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/landing/css/odometer.css" />
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/landing/css/owl.min.css" />
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/admin/css/toastr.css" />
+    <link rel="stylesheet" href="https://d20jjxed5popc9.cloudfront.net/assets/landing/css/main.css" />
 
     @php($icon = \App\Models\BusinessSetting::where(['key' => 'icon'])->first()->value ?? '')
     <link rel="icon" type="image/x-icon" href="{{ asset('/assets/storage/app/public/business/' . $icon ?? asset('/assets/landing/img/favicon.svg')) }}">

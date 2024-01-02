522e66e44b6902dc46a50e8df3019ed7a10d1a25
zawwaisoe-lastoenjoy <zawwaisoe.lastoenjoy@gmail.com>
2024-01-02 15:00:40 +0630

    add equiv in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,7 @@
 <head>
     <!-- Required Meta Tags Always Come First -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Title -->
     <title>{{translate('messages.login')}}</title>
